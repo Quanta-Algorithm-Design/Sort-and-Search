646f2da66d474115d4525b1b424f615f2782cafc
Ali Mahani <49623034+AliMahani@users.noreply.github.com>
2020-09-04 03:44:13 +0430

    Optimized the usage of arrays

--- count.py
+++ count.py
@@ -11,28 +11,28 @@
 # int from 0 to k-1, and then uses an algorithm to find the number
 # of items -LE to the item of the index. Then it uses that information
 # to sort the whole thing in linear time.
+# This is a practice of "Stable Sorting" which will come in handy for Radix Sort
 
 from generator import generateList
 
-def countSort(listA):
+def countSort(k, n, arrA):
     # Initialize a list of size 200 with values 0. This is the count.
-    pos = [0] * 200
+    pos = [0] * k
 
     # Count the occurence of each int.
-    for item in listA:
+    for item in arrA:
         pos[item] += 1
 
-    # Find the number of items -LE the int and store it instead of count.
-    for i in range(1, len(pos)):
+    # Find the number of items -LE the item and store it instead of count.
+    for i in range(1, upBound):
         pos[i] += pos[i-1]
 
-    n = len(listA)
     out = [0] * n
 
     # Find the position of each item from pos map it in out
     for index in range(n-1, -1, -1):
-        pos[listA[index]] -= 1
-        out[pos[listA[index]]] = listA[index]
+        pos[arrA[index]] -= 1
+        out[pos[arrA[index]]] = arrA[index]
 
     return out
 
@@ -41,9 +41,9 @@ def main():
     inList = []
     up, size, arr = generateList()
     for item in arr:
-        inList.append(int(item))
+        inList.append(item)
 
-    print(countSort(inList))
+    print(countSort(up, size, inList))
     return 0
 
 # Profiler to benchmark the output for performance test.
