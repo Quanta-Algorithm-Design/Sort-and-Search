a5812b9082a77294a2105b247208f7e79710edec
Ali Mahani <49623034+AliMahani@users.noreply.github.com>
2020-09-04 15:56:29 +0430

    Create generator.py

---
+++ Counting Sort/generator.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def generateList():
+    """
+    Get the length from user and generate a random array of that size
+    """
+    upperBound = int(input("-> Please choose the Upper Bound of the array: "))
+    size = int(input("-> Please choose the size of the array: "))
+
+    return upperBound, size, np.random.randint(0, upperBound, size)
