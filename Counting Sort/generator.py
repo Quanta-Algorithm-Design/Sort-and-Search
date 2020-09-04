2253fb5e9c5cc68aa84d9d62cd36b9a40fb15e93
Ali Mahani <49623034+AliMahani@users.noreply.github.com>
2020-09-04 17:05:15 +0430

    faster generation with fastrand library

--- Counting Sort/generator.py
+++ Counting Sort/generator.py
@@ -1,10 +1,16 @@
-import numpy as np
+from fastrand import pcg32bounded   # A C-Level random generator (costs 1/10 of randint)
 
-def generateList():
+def generate_list():
     """
     Get the length from user and generate a random array of that size
     """
-    upperBound = int(input("-> Please choose the Upper Bound of the array: "))
+    upper_bound = int(input("-> Please choose the Upper Bound of the array: "))
     size = int(input("-> Please choose the size of the array: "))
 
-    return upperBound, size, np.random.randint(0, upperBound, size)
+    # return upperBound, size, np.random.randint(0, upperBound, size)
+
+    rand_arr = []
+    for _ in range(size):
+        rand_arr.append(pcg32bounded(upper_bound))
+
+    return upper_bound, size, rand_arr
